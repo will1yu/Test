c2ba4d5876d55b47d0933d743ec4ddb231f25b9d
Will Yu <will@WilldeMac.local>
2018-01-25 00:59:37 +0800

    【feature】update lesson_02

--- Swift_02.playground/Contents.swift
+++ Swift_02.playground/Contents.swift
@@ -8,4 +8,8 @@ let http404Error = (404, "Not Found")
 // http404Error 的类型为(Int, String)
 
 
+let (statusCode, statusMessage) = http404Error
+print("状态码是 \(statusCode)")
+NSLog("状态消息是 %@", statusMessage)
+
 
