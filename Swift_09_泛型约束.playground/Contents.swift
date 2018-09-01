ab34bddc3a52f30a5273e9f6c72fd98e3d26d8b2
Will Yu <will@WilldeMac.local>
2018-09-01 16:20:32 +0800

    【Feature】更新泛型约束

--- Swift_09_泛型约束.playground/Contents.swift
+++ Swift_09_泛型约束.playground/Contents.swift
@@ -2,11 +2,41 @@
 
 import UIKit
 
-var moneyList = [1, 3, 10, 5, 4]
-var monetStrList = moneyList.map { (item) -> T in
-    return "\(item)?"
+/////////////
+/// 泛型
+func swap<T>( a: inout T, b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
 }
 
+func testOne() {
+    var someInt = 3
+    var anotherInt = 107
+    // 给inout参数传参时，不要忘记加（&）
+    swap(&someInt, &anotherInt)
+    print("\(someInt) - \(anotherInt)")
+    
+    var someStr = "123"
+    var anotherStr = "456"
+    swap(&someStr, &anotherStr)
+    print("\(someStr) - \(anotherStr)")
+}
+testOne()
+
+var moneyList = [1, 3, 10, 5, 4]
+var moneyStrListA = moneyList.map { "\($0)$" }
+var moneyStrListB = moneyList.map { item -> String in
+    return "\(item)!"
+}
+
+
+////////////
+/// 约束
+
+func isEqual<T:Equatable>(left: T, right: T) -> Bool {
+    return left == right
+}
 
 extension Collection where Element == String {
     func countWords() -> Int {
@@ -19,3 +49,4 @@ extension Collection where Element == String {
 
 let array2 = ["sunny", "cloudy", "apple orange"]
 array2.countWords()
+
